027f98a977b4a7e8bc37001a349c882aa8099696
abuhasan-Ohovro <abuhasanmondal24@gmail.com>
2024-08-30 19:31:32 +0600

    HTML  & CSS

---
+++ C++ Code/01_Basics/01_Bascis.cpp
@@ -0,0 +1,16 @@
+//? Writhing Hello wolrd in C++
+
+#include<iostream>  //* this is called library header file .  //* standard library header for input / output
+using namespace std;
+int main(){
+ const int age = 323;
+    
+    // const int 
+
+    cout << " Hello World!" << endl;
+
+    
+    age = 3;
+    return 0 ;
+}
+
